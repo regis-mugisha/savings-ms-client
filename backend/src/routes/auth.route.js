804561bc81792fa2a363955af1d72bd5f9bafe37
regis-mugisha <mugisharegis72@gmail.com>
2025-10-28 11:34:33 +0200

    fix(auth): use correct authenticate middleware in auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { registerUser, loginUser, refreshUserToken } from "../controllers/auth.controller.js";
+import { registerUser, loginUser, refreshUserToken, updatePushToken } from "../controllers/auth.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
 import Joi from "joi";
 import validate from "../middlewares/validation.js";
 
@@ -22,9 +23,14 @@ const refreshTokenSchema = Joi.object({
     refreshToken: Joi.string().required(),
 })
 
+const updatePushTokenSchema = Joi.object({
+    pushToken: Joi.string().required(),
+})
+
 // Routes
 router.post("/register", validate(registerSchema), registerUser);
 router.post("/login", validate(loginSchema), loginUser);
 router.post("/refresh", validate(refreshTokenSchema), refreshUserToken);
+router.post("/push-token", authenticate, validate(updatePushTokenSchema), updatePushToken);
 
 export default router;
