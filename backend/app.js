b11178ee7724282d77d64bcab059d7a58991f308
regis-mugisha <mugisharegis72@gmail.com>
2025-10-28 20:27:05 +0200

    feat: add CORS policy to allow requests from admin panel

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,9 @@ import connectDB from "./src/config/db.config.js";
 import authRoutes from "./src/routes/auth.route.js";
 import savingsRoutes from "./src/routes/savings.route.js";
 import adminRoutes from "./src/routes/admin.route.js";
-import mongoSanitize from 'express-mongo-sanitize';
+import mongoSanitize from "express-mongo-sanitize";
 import { seedAdmin } from "./src/utils/seed-admin.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 6000;
@@ -13,20 +14,26 @@ const PORT = process.env.PORT || 6000;
 await connectDB();
 await seedAdmin();
 
+app.use(
+  cors({
+    origin: ["https://savings-ms-admin-panel.vercel.app"],
+    credentials: "true",
+  })
+);
+
 app.use(mongoSanitize());
 app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Savings Management System API:v1")
-})
-app.get("/api/v1/health", (req,res) => {
-  res.status(200).send("OK")
-} )
+  res.send("Savings Management System API:v1");
+});
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).send("OK");
+});
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/savings", savingsRoutes);
 app.use("/api/v1/admin", adminRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
